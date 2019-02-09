05547e5b6b0effca6b05f0cbda33c86fd120c83e
Alan Evans <thealanevans@gmail.com>
2019-02-09 10:06:02 -0400

    Reformat

--- app/src/test/java/io/github/westonal/alansgiphysearch/ExampleUnitTest.java
+++ app/src/test/java/io/github/westonal/alansgiphysearch/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package io.github.westonal.alansgiphysearch;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
